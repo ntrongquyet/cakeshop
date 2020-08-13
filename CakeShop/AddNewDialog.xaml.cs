01c8e1cce930abd5bae1f4437db7489c369f5025
TỐ NGÔ TẤT <18600014@student.hcmus.edu.vn>
2020-08-13 22:37:27 +0700

    edit add_food

--- CakeShop/AddNewDialog.xaml.cs
+++ CakeShop/AddNewDialog.xaml.cs
@@ -34,108 +34,108 @@ namespace CakeShop
         }
         string pathImage = ""; // Lưu đường dẫn bánh
 
-        private void Add_Button(object sender, RoutedEventArgs e)
-        {
-            var ID = listCombobox.SelectedValue as LOAIBANH; // Lưu mã bánh
-            double money = 0; // Lưu giá tiền
-            int amount = 0; // Lưu số lượng bánh
-            int.TryParse(count.Text.Trim(), out amount);
-            double.TryParse(price.Text.Trim(), out money);
-            if (listCombobox.SelectedIndex == -1)
-            {
-                MessageBox.Show("Chưa chọn loại bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
-            }
-            else if (name.Text.Trim().Length == 0)
-            {
-                MessageBox.Show("Chưa nhập tên bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //private void add_button(object sender, routedeventargs e)
+        //{
+        //    var id = listcombobox.selectedvalue as loaibanh; // lưu mã bánh
+        //    double money = 0; // lưu giá tiền
+        //    int amount = 0; // lưu số lượng bánh
+        //    int.tryparse(count.text.trim(), out amount);
+        //    double.tryparse(price.text.trim(), out money);
+        //    if (listcombobox.selectedindex == -1)
+        //    {
+        //        messagebox.show("chưa chọn loại bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
+        //    }
+        //    else if (name.text.trim().length == 0)
+        //    {
+        //        messagebox.show("chưa nhập tên bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
 
-            }
-            else if (ingredients.Text.Trim().Length == 0)
-            {
-                MessageBox.Show("Chưa nhập thông tin cho bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    }
+        //    else if (ingredients.text.trim().length == 0)
+        //    {
+        //        messagebox.show("chưa nhập thông tin cho bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
 
-            }
+        //    }
 
-            else if (amount == 0)
-            {
-                MessageBox.Show("Sai định dạng số lượng bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    else if (amount == 0)
+        //    {
+        //        messagebox.show("sai định dạng số lượng bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
 
-            }
-            else if (money == 0 && money<1000)
-            {
-                MessageBox.Show("Sai định dạng số tiền", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    }
+        //    else if (money == 0 && money<1000)
+        //    {
+        //        messagebox.show("sai định dạng số tiền", "lỗi", messageboxbutton.ok, messageboximage.warning);
 
-            }
-            else if (unit.Text.Trim().Length == 0)
-            {
-                MessageBox.Show("Chưa nhập đơn vị tính", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    }
+        //    else if (unit.text.trim().length == 0)
+        //    {
+        //        messagebox.show("chưa nhập đơn vị tính", "lỗi", messageboxbutton.ok, messageboximage.warning);
 
-            }
-            else if (pathImage == "")
-            {
-                MessageBox.Show("Chưa thêm hình ảnh cho bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+        //    }
+        //    else if (pathimage == "")
+        //    {
+        //        messagebox.show("chưa thêm hình ảnh cho bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
 
-            }else
-            {
-                var info = new FileInfo(pathImage);
-                string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
-                baseFolder += "Image\\Cakes\\";
-                string nameImage = Guid.NewGuid().ToString();
-                File.Copy(pathImage, baseFolder + nameImage + info.Extension);
-                var newCake = new BANH
-                {
-                    ID = DataProvider.Ins.DB.BANHs.Count() + 1,
-                    MABANH = $"BA{DataProvider.Ins.DB.BANHs.Count() + 1}",
-                    TENBANH = name.Text.Trim(),
-                    SL_TON = amount,
-                    DONGIA = money,
-                    DVT = unit.Text.Trim(),
-                    HA_BANH = $"Cakes\\{nameImage}",
-                    LOAIBANH = ID.MALOAI,
-                    THONGTIN = ingredients.Text.Trim()
+        //    }else
+        //    {
+        //        var info = new fileinfo(pathimage);
+        //        string basefolder = appdomain.currentdomain.basedirectory;
+        //        basefolder += "image\\cakes\\";
+        //        string nameimage = guid.newguid().tostring();
+        //        file.copy(pathimage, basefolder + nameimage + info.extension);
+        //        var newcake = new banh
+        //        {
+        //            id = dataprovider.ins.db.banhs.count() + 1,
+        //            mabanh = $"ba{dataprovider.ins.db.banhs.count() + 1}",
+        //            tenbanh = name.text.trim(),
+        //            sl_ton = amount,
+        //            dongia = money,
+        //            dvt = unit.text.trim(),
+        //            ha_banh = $"cakes\\{nameimage}",
+        //            loaibanh = id.maloai,
+        //            thongtin = ingredients.text.trim()
                     
 
 
-                };
-                DataProvider.Ins.DB.BANHs.Add(newCake);
-                DataProvider.Ins.DB.SaveChanges();
-                resetTextBox();
-                MessageBox.Show($"Thêm thành công món {newCake.TENBANH}", "Thành công", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        //        };
+        //        dataprovider.ins.db.banhs.add(newcake);
+        //        dataprovider.ins.db.savechanges();
+        //        resettextbox();
+        //        messagebox.show($"thêm thành công món {newcake.tenbanh}", "thành công", messageboxbutton.ok, messageboximage.exclamation);
 
-            }
-        }
-        private void resetTextBox()
-        {
-            listCombobox.SelectedIndex = default;
-            name.Text = default;
-            ingredients.Text = default;
-            count.Text = default;
-            price.Text = default;
-            unit.Text = default;
-        }
-        private void Image_Button(object sender, MouseButtonEventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.Multiselect = false;
-            ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
-            if (ofd.ShowDialog() == true)
-            {
-                pathImage = ofd.FileName;
-            }
-            if (pathImage != "")
-            {
-                BitmapImage logo = new BitmapImage();
-                logo.BeginInit();
-                logo.UriSource = new Uri(pathImage);
-                logo.EndInit();
-                cakeImage.Source = logo;
-            }
-        }
+        //    }
+        //}
+        //private void resettextbox()
+        //{
+        //    listcombobox.selectedindex = default;
+        //    name.text = default;
+        //    ingredients.text = default;
+        //    count.text = default;
+        //    price.text = default;
+        //    unit.text = default;
+        //}
+        //private void image_button(object sender, mousebuttoneventargs e)
+        //{
+        //    openfiledialog ofd = new openfiledialog();
+        //    ofd.multiselect = false;
+        //    ofd.filter = "image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+        //    if (ofd.showdialog() == true)
+        //    {
+        //        pathimage = ofd.filename;
+        //    }
+        //    if (pathimage != "")
+        //    {
+        //        bitmapimage logo = new bitmapimage();
+        //        logo.begininit();
+        //        logo.urisource = new uri(pathimage);
+        //        logo.endinit();
+        //        cakeimage.source = logo;
+        //    }
+        //}
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            listCombobox.ItemsSource = DataProvider.Ins.DB.LOAIBANHs.ToList();
-        }
+        //private void window_loaded(object sender, routedeventargs e)
+        //{
+        //    listcombobox.itemssource = dataprovider.ins.db.loaibanhs.tolist();
+        //}
 
         private void Window_Closed(object sender, EventArgs e)
         {
@@ -145,6 +145,21 @@ namespace CakeShop
         private void Drag_Window(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void Image_Button(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Add_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void out_button(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
         }
     }
 }
