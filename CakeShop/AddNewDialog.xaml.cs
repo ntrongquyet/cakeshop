bfd8ab37e25bf492f9d98b2c7159b2a776d21bed
ntrongquyet <ntrongquyet37@gmail.com>
2020-08-15 12:22:15 +0700

    commit

--- CakeShop/AddNewDialog.xaml.cs
+++ CakeShop/AddNewDialog.xaml.cs
@@ -30,136 +30,150 @@ namespace CakeShop
 
         private void Out_button(object sender, RoutedEventArgs e)
         {
-              this.Close();
+            this.Close();
         }
         string pathImage = ""; // Lưu đường dẫn bánh
 
-        //private void add_button(object sender, routedeventargs e)
+        //private void Drag_Window(object sender, MouseButtonEventArgs e)
         //{
-        //    var id = listcombobox.selectedvalue as loaibanh; // lưu mã bánh
-        //    double money = 0; // lưu giá tiền
-        //    int amount = 0; // lưu số lượng bánh
-        //    int.tryparse(count.text.trim(), out amount);
-        //    double.tryparse(price.text.trim(), out money);
-        //    if (listcombobox.selectedindex == -1)
-        //    {
-        //        messagebox.show("chưa chọn loại bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
-        //    }
-        //    else if (name.text.trim().length == 0)
-        //    {
-        //        messagebox.show("chưa nhập tên bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
-
-        //    }
-        //    else if (ingredients.text.trim().length == 0)
-        //    {
-        //        messagebox.show("chưa nhập thông tin cho bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
-
-        //    }
-
-        //    else if (amount == 0)
-        //    {
-        //        messagebox.show("sai định dạng số lượng bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
-
-        //    }
-        //    else if (money == 0 && money<1000)
-        //    {
-        //        messagebox.show("sai định dạng số tiền", "lỗi", messageboxbutton.ok, messageboximage.warning);
-
-        //    }
-        //    else if (unit.text.trim().length == 0)
-        //    {
-        //        messagebox.show("chưa nhập đơn vị tính", "lỗi", messageboxbutton.ok, messageboximage.warning);
-
-        //    }
-        //    else if (pathimage == "")
-        //    {
-        //        messagebox.show("chưa thêm hình ảnh cho bánh", "lỗi", messageboxbutton.ok, messageboximage.warning);
-
-        //    }else
-        //    {
-        //        var info = new fileinfo(pathimage);
-        //        string basefolder = appdomain.currentdomain.basedirectory;
-        //        basefolder += "image\\cakes\\";
-        //        string nameimage = guid.newguid().tostring();
-        //        file.copy(pathimage, basefolder + nameimage + info.extension);
-        //        var newcake = new banh
-        //        {
-        //            id = dataprovider.ins.db.banhs.count() + 1,
-        //            mabanh = $"ba{dataprovider.ins.db.banhs.count() + 1}",
-        //            tenbanh = name.text.trim(),
-        //            sl_ton = amount,
-        //            dongia = money,
-        //            dvt = unit.text.trim(),
-        //            ha_banh = $"cakes\\{nameimage}",
-        //            loaibanh = id.maloai,
-        //            thongtin = ingredients.text.trim()
-                    
-
-
-        //        };
-        //        dataprovider.ins.db.banhs.add(newcake);
-        //        dataprovider.ins.db.savechanges();
-        //        resettextbox();
-        //        messagebox.show($"thêm thành công món {newcake.tenbanh}", "thành công", messageboxbutton.ok, messageboximage.exclamation);
-
-        //    }
+        //    this.DragMove();
         //}
-        //private void resettextbox()
-        //{
-        //    listcombobox.selectedindex = default;
-        //    name.text = default;
-        //    ingredients.text = default;
-        //    count.text = default;
-        //    price.text = default;
-        //    unit.text = default;
-        //}
-        //private void image_button(object sender, mousebuttoneventargs e)
-        //{
-        //    openfiledialog ofd = new openfiledialog();
-        //    ofd.multiselect = false;
-        //    ofd.filter = "image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
-        //    if (ofd.showdialog() == true)
-        //    {
-        //        pathimage = ofd.filename;
-        //    }
-        //    if (pathimage != "")
-        //    {
-        //        bitmapimage logo = new bitmapimage();
-        //        logo.begininit();
-        //        logo.urisource = new uri(pathimage);
-        //        logo.endinit();
-        //        cakeimage.source = logo;
-        //    }
-        //}
-
-        //private void window_loaded(object sender, routedeventargs e)
-        //{
-        //    listcombobox.itemssource = dataprovider.ins.db.loaibanhs.tolist();
-        //}
-
-        private void Window_Closed(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void Drag_Window(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
 
         private void Image_Button(object sender, MouseButtonEventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (ofd.ShowDialog() == true)
+            {
+                pathImage = ofd.FileName;
+            }
+            if (pathImage != "")
+            {
+                BitmapImage logo = new BitmapImage();
+                logo.BeginInit();
+                logo.UriSource = new Uri(pathImage);
+                logo.EndInit();
+                cakeImage.Source = logo;
+            }
         }
+        public int getIndexSelected(ItemCollection items)
+        {
+            int index = -1;
 
+            foreach (RadioButton item in items)
+            {
+                index++;
+                if (item.IsChecked == true)
+                {
+                    return index;
+
+                }
+            }
+            return index;
+        }
+        protected LOAIBANH temp;
         private void Add_Button(object sender, RoutedEventArgs e)
         {
+            
+            double money = 0; // Lưu giá tiền
+            int amount = 0; // Lưu số lượng bánh
+            int.TryParse(count.Text.Trim(), out amount);
+            double.TryParse(price.Text.Trim(), out money);
+            if (temp == null)
+            {
+                MessageBox.Show("Chưa chọn loại bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (name.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Chưa nhập tên bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
 
+            }
+            else if (ingredients.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Chưa nhập thông tin cho bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+
+            else if (amount == 0)
+            {
+                MessageBox.Show("Sai định dạng số lượng bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else if (money == 0 && money < 1000)
+            {
+                MessageBox.Show("Sai định dạng số tiền", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else if (unit.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Chưa nhập đơn vị tính", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else if (pathImage == "")
+            {
+                MessageBox.Show("Chưa thêm hình ảnh cho bánh", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            else
+            {
+                var info = new FileInfo(pathImage);
+                string baseFolder = AppDomain.CurrentDomain.BaseDirectory;
+                baseFolder += "Image\\Cakes\\";
+                string nameImage = Guid.NewGuid().ToString();
+                File.Copy(pathImage, baseFolder + nameImage + info.Extension);
+                var newCake = new BANH
+                {
+                    ID = DataProvider.Ins.DB.BANHs.Count() + 1,
+                    MABANH = $"BA{DataProvider.Ins.DB.BANHs.Count() + 1}",
+                    TENBANH = name.Text.Trim(),
+                    SL_TON = amount,
+                    DONGIA = money,
+                    DVT = unit.Text.Trim(),
+                    HA_BANH = $"Cakes\\{nameImage}",
+                    LOAIBANH = temp.MALOAI,
+                    THONGTIN = ingredients.Text.Trim()
+
+
+
+                };
+                DataProvider.Ins.DB.BANHs.Add(newCake);
+                DataProvider.Ins.DB.SaveChanges();
+                resetTextBox();
+                MessageBox.Show($"Thêm thành công món {newCake.TENBANH}", "Thành công", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+            }
         }
-
+        private void resetTextBox()
+        {
+            listCombobox.SelectedIndex = default;
+            name.Text = default;
+            ingredients.Text = default;
+            count.Text = default;
+            price.Text = default;
+            unit.Text = default;
+        }
         private void out_button(object sender, MouseButtonEventArgs e)
         {
             this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            listCombobox.ItemsSource = DataProvider.Ins.DB.LOAIBANHs.ToList();
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string name = "";
+            if (((RadioButton)sender).IsChecked == true)
+            {
+                // This is the correct control.
+                RadioButton rb = (RadioButton)sender;
+                name = rb.Content.ToString();
+            }
+            temp = DataProvider.Ins.DB.LOAIBANHs.ToList().Find(x=>x.TENLOAI == name);
         }
     }
 }
--- CakeShop/User_Control/StatisticsUCVers2.xaml.cs
+++ CakeShop/User_Control/StatisticsUCVers2.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Helpers;
+using LiveCharts.Wpf;
+using LiveCharts.Wpf.Charts.Base;
+using CakeShop.SQL;
 
 namespace CakeShop.User_Control
 {
@@ -20,9 +25,122 @@ namespace CakeShop.User_Control
     /// </summary>
     public partial class StatisticsUCVers2 : UserControl
     {
+        public SeriesCollection SeriesCollection_MoneyPerDay { get; }
+        public SeriesCollection SeriesCollection_TypeOfCake { get; set; }
+
+        public SeriesCollection SeriesCollection_MoneyPerMonth { get; set; }
+        public List<string> Labels { get; set; } = new List<string>();
         public StatisticsUCVers2()
         {
             InitializeComponent();
+            InitializeComponent();
+            // Xuất biểu đồ số loại bánh còn tồn kho
+            SeriesCollection_MoneyPerDay = new SeriesCollection();
+            var listInventory = from banh in DataProvider.Ins.DB.BANHs
+                                where banh.SL_TON > 0
+                                select banh;
+            foreach (BANH item in listInventory)
+            {
+                ChartValues<int> amount = new ChartValues<int>();
+                amount.Add(Convert.ToInt32(item.SL_TON));
+                PieSeries series = new PieSeries
+                {
+                    Values = amount,
+                    Title = item.TENBANH,
+                };
+                SeriesCollection_MoneyPerDay.Add(series);
+            }
+            // Hiện thị biểu đồ doanh thu tháng
+            var listOfMonth = DataProvider.Ins.DB.DONHANGs.ToList(); // Danh sách tất cả các đơn hàng
+            double[] sum = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            // Tính tổng số tiền thu được theo từng tháng
+            foreach (DONHANG item in listOfMonth)
+            {
+                var getMonth = item.NG_DATHANG.Value.ToString("MM");
+                var yearDefault = "2020";
+                var getYear = item.NG_DATHANG.Value.ToString("yyyy");
+                if (getYear == yearDefault)
+                {
+                    sum[Convert.ToInt32(getMonth) - 1] += Convert.ToDouble(item.TONG_GTDH);
+
+                }
+            }
+            SeriesCollection_MoneyPerMonth = new SeriesCollection();
+            for (int i = 0; i < sum.Length; i++)
+            {
+                ColumnSeries series;
+                if (sum[i] > 0)
+                {
+                    ChartValues<double> moneyOfMonth = new ChartValues<double>();
+                    moneyOfMonth.Add(sum[i]);
+                    series = new ColumnSeries
+                    {
+                        Title = $"Tháng {i + 1}",
+                        Values = moneyOfMonth
+
+                    };
+                    SeriesCollection_MoneyPerMonth.Add(series);
+                    Labels.Add("2020");
+                }
+
+            }
+            Chart();
+        }
+        public void Chart()
+        {
+            SeriesCollection_TypeOfCake = new SeriesCollection();
+
+            var db = DataProvider.Ins.DB;
+
+            var list = (from ctdh in db.CT_DONHANG
+                        join banh in db.BANHs on ctdh.MABANH equals banh.MABANH
+                        select new { banh.LOAIBANH, total = ctdh.SL_MUA * banh.DONGIA });
+            var result = (from l in list
+                          join lb in db.LOAIBANHs on l.LOAIBANH equals lb.MALOAI
+                          select new { lb.TENLOAI, l.total });
+            var finalList = (from rs in result
+                             group rs by rs.TENLOAI into rsGroup
+                             select new
+                             {
+                                 Name = rsGroup.Key,
+                                 totalMoney = rsGroup.Sum(x => x.total)
+                             }).ToList();
+            for (int i = 0; i < finalList.Count; i++)
+            {
+                ChartValues<int> amount = new ChartValues<int>();
+                amount.Add(Convert.ToInt32(finalList[i].totalMoney));
+                PieSeries series = new PieSeries
+                {
+                    Values = amount,
+                    Title = finalList[i].Name,
+                };
+                SeriesCollection_TypeOfCake.Add(series);
+            }
+        }
+        /// <summary>
+        /// Khởi động usercontrol
+        /// </summary>
+        public void UserControl_Initialized(object sender, EventArgs e)
+        {
+            // Hiện thị đơn hàng bán được trong ngày
+            var dateNow = DateTime.Now.ToString("d");
+            DateTime dateTime = Convert.ToDateTime(dateNow);
+            totalBillInDay.Text = (from dh in DataProvider.Ins.DB.DONHANGs
+                                      where dh.NG_DATHANG == dateTime
+                                      select dh).Count().ToString();
+            // Hiện thị số tiền trong 1 ngày
+            var total = (from dh in DataProvider.Ins.DB.DONHANGs
+                         where dh.NG_DATHANG == dateTime
+                         select dh.TONG_GTDH).Sum();
+            totalMoneyInDay.Text = $"{string.Format("{0:n0}", total)} VNĐ";
+            if (totalMoneyInDay.Text == null)
+            {
+                totalMoneyInDay.Text = $"{string.Format("{0:n0}", 0)} VNĐ";
+            }
+            //Hiện danh sách các loại bánh còn trong kho
+            table_Inventory.ItemsSource = (from banh in DataProvider.Ins.DB.BANHs
+                                           where banh.SL_TON > 0
+                                           select banh).ToList();
         }
     }
 }
